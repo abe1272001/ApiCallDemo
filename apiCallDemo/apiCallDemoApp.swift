12a30471afcf8cdc7a0d9190848dc2bc3978a085
Abe Chen <abe1272001@gmail.com>
2022-07-20 10:49:06 +0800

    Initial Commit

---
+++ apiCallDemo/apiCallDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiCallDemoApp.swift
+//  apiCallDemo
+//
+//  Created by abe chen on 2022/7/20.
+//
+
+import SwiftUI
+
+@main
+struct apiCallDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
